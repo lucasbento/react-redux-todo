285bae9a7520e3ff9409f497929e93d414ebf9e5
Lucas Bento <lucas.bsilva@outlook.com>
2016-11-12 17:06:19 -0200

    Add redux-persist to persist the state

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist'
 import reducers from '../reducers';
 
-export default createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+
+function configureStore() {
+  const enhancer = compose(autoRehydrate(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+  const store = createStore(reducers, enhancer);
+
+  persistStore(store);
+
+  return store;
+}
+
+export default configureStore();
